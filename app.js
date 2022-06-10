d4ab46d375c91095cbdd677673f41577fb5c28df
Ldi569321 <81796049+Ldi569321@users.noreply.github.com>
2022-06-10 13:18:34 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let input = document.querySelector("input");
+let num1;
+let num2;
+
+const one = document.querySelector("#1")
+one.addEventListener("click", ()=>{
+    input
+})
+const two = document.querySelector("#2")
+const three = document.querySelector("#3")
+const four = document.querySelector("#4")
+const five = document.querySelector("#5")
+const six = document.querySelector("#6")
+const seven = document.querySelector("#7")
+const eight = document.querySelector("#8")
+const nine = document.querySelector("#9")
