bc5c60c6822ed24986270c1d258e7623ad2d66c8
Ldi569321 <dragon569321@naver.com>
2022-06-12 17:42:05 +0900

    0612_17:42

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let input = document.querySelector("input");
+let input = document.querySelectorAll("input");
 let num1;
 let num2;
 
 function maxlength() {
-    if (input.value.length >= 9) {
+    if (input[2].value.length >= 9) {
         alert("숫자는 9자릿수를 넘을수 없습니다.");
         일부러낸오류코드;
     };
@@ -12,101 +12,117 @@ function maxlength() {
 const one = document.querySelector("#one");
 one.addEventListener("click", () => {
     maxlength()
-    input.value += `1`;
+    input[2].value += `1`;
 });
 
 const two = document.querySelector("#two");
 two.addEventListener("click", () => {
     maxlength()
-    input.value += `2`;
+    input[2].value += `2`;
 });
 
 const three = document.querySelector("#three");
 three.addEventListener("click", () => {
     maxlength()
-    input.value += `3`;
+    input[2].value += `3`;
 });
 
 const four = document.querySelector("#four");
 four.addEventListener("click", () => {
     maxlength()
-    input.value += `4`;
+    input[2].value += `4`;
 });
 
 const five = document.querySelector("#five");
 five.addEventListener("click", () => {
     maxlength()
-    input.value += `5`;
+    input[2].value += `5`;
 });
 
 const six = document.querySelector("#six");
 six.addEventListener("click", () => {
     maxlength()
-    input.value += `6`;
+    input[2].value += `6`;
 });
 
 const seven = document.querySelector("#seven");
 seven.addEventListener("click", () => {
     maxlength()
-    input.value += `7`;
+    input[2].value += `7`;
 });
 
 const eight = document.querySelector("#eight");
 eight.addEventListener("click", () => {
     maxlength()
-    input.value += `8`;
+    input[2].value += `8`;
 });
 
 const nine = document.querySelector("#nine");
 nine.addEventListener("click", () => {
     maxlength()
-    input.value += `9`;
+    input[2].value += `9`;
+});
+
+const zero = document.querySelector("#zero");
+zero.addEventListener("click", () => {
+    maxlength()
+    input[2].value += `0`;
 });
 
 let sign = 0;
-
+let num = document.querySelector("#num");
 const plus = document.querySelector("#plus");
 plus.addEventListener("click", () => {
-    num1 = (Number(input.value));
-    input.value = ``;
+    num1 = (Number(input[2].value));
+    input[2].value = ``;
     sign = 1;
+    num.value = num1;
+    input[1].value = "+";
 })
 
 const minus = document.querySelector("#minus");
 minus.addEventListener("click", () => {
-    num1 = (Number(input.value));
-    input.value = ``;
+    num1 = (Number(input[2].value));
+    input[2].value = ``;
     sign = 2;
+    num.value = num1;
+    input[1].value = `-`;
 })
 
 const multifly = document.querySelector("#multifly")
 multifly.addEventListener("click", () => {
-    num1 = (Number(input.value));
-    input.value = ``;
+    num1 = (Number(input[2].value));
+    input[2].value = ``;
     sign = 3;
+    num.value = num1;
+    input[1].value = `×`;
 })
 
 const devide = document.querySelector("#devide");
 devide.addEventListener("click", () => {
-    num1 = (Number(input.value));
-    input.value = ``;
+    num1 = (Number(input[2].value));
+    input[2].value = ``;
     sign = 4;
+    num.value = num1;
+    input[1].value = `÷`;
 })
 
 const result = document.querySelector("#result");
 let resultNum;
 result.addEventListener("click", () => {
-    num2 = (Number(input.value));
+    num2 = (Number(input[2].value));
     switch (sign) {
-        case 1: input.value = num1 + num2;
+        case 1: input[2].value = num1 + num2;
             break;
-        case 2: input.value = num1 - num2;
+        case 2: input[2].value = num1 - num2;
             break;
-        case 3: input.value = num1 * num2;
+        case 3: input[2].value = num1 * num2;
             break;
-        case 4: input.value = num1 / num2;
+        case 4: input[2].value = num1 / num2;
             break;
     }
+    num.value = ``;
+    input[1].value = ``;
 
 })
 
@@ -114,11 +130,11 @@ const clear = document.querySelector("#C");
 clear.addEventListener("click", () => {
     num1 = 0;
     num2 = 0;
-    input.value = ``;
+    input[2].value = ``;
 })
 
 const del = document.querySelector("#del");
 del.addEventListener("click", () => {
-    delResult = input.value.slice(-input.value.length, input.value.length - 1);
-    input.value = delResult;
+    delResult = input[2].value.slice(-input[2].value.length, input[2].value.length - 1);
+    input[2].value = delResult;
 })
